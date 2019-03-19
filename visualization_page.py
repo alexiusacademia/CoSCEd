6dda3aed2038196357c1358729be070f6c3a6d65
Alexius Academia <alexius.academia@gmail.com>
2019-03-20 07:23:13 +0800

    Added vertical axis. Displayed the completion days.

--- visualization_page.py
+++ visualization_page.py
@@ -16,7 +16,7 @@ class Timeline(tk.Frame):
 
     canvas_top_margin = 30
     canvas_bottom_margin = 30
-    canvas_left_margin = 30
+    canvas_left_margin = 40
     canvas_right_margin = 30
 
     def __init__(self, parent):
@@ -34,8 +34,21 @@ class Timeline(tk.Frame):
         self.slippage = 0
 
         # ===========================================================
+        # String Variables
+        self.str_cdp_time = tk.StringVar()
+        self.str_cdp_accomp = tk.StringVar()
+        self.str_slippage = tk.StringVar()
+        self.str_summary_total_suspended = tk.StringVar()
+        self.str_summary_total_suspension_order = tk.StringVar()
+        self.str_summ_orig_completion_days = tk.StringVar()
+        self.str_summ_rev_completion_days = tk.StringVar()
+
+        self.init_ui()
+
+    def init_ui(self):
+        # ===========================================================
         # Menus
-        menu_bar = tk.Menu(parent)
+        menu_bar = tk.Menu(self.parent)
 
         menu_save = tk.Menu(menu_bar, tearoff=0)
         menu_save.add_command(label='Save S-Curve', command=self.save_scurve)
@@ -44,23 +57,17 @@ class Timeline(tk.Frame):
         file_menu.add_command(label='New')
         file_menu.add_cascade(label='Save', menu=menu_save)
         file_menu.add_separator()
-        file_menu.add_command(label='Quit', command=parent.quit)
+        file_menu.add_command(label='Quit', command=self.parent.quit)
 
         menu_bar.add_cascade(label="File", menu=file_menu)
 
-        parent.config(menu=menu_bar)
+        self.parent.config(menu=menu_bar)
 
         canvas_title = tk.Label(self, text="Time Frame")
         canvas_title.grid(column=1, row=0)
 
         # ===========================================================
-        # String Variables
-        self.str_cdp_time = tk.StringVar()
-        self.str_cdp_accomp = tk.StringVar()
-        self.str_summary_total_suspended = tk.StringVar()
-        self.str_summary_total_suspension_order = tk.StringVar()
-        self.str_slippage = tk.StringVar()
-        # ===========================================================
+        # Left panel for displaying of textual data.
         left_panel = tk.Frame(self)
         left_panel.grid(row=0, column=0, rowspan=2, sticky='nesw')
 
@@ -71,15 +78,15 @@ class Timeline(tk.Frame):
 
         cdp_time_label = tk.Label(frame_canvas_data_display, text="Time").grid(row=0, column=0, sticky="nw")
         cdp_time = tk.Label(frame_canvas_data_display, text='', width=10, anchor='ne',
-                            relief='sunken', textvariable=self.str_cdp_time)\
+                            relief='sunken', textvariable=self.str_cdp_time) \
             .grid(row=0, column=1, sticky="nesw", padx=5, pady=5)
         cdp_accomp_label = tk.Label(frame_canvas_data_display, text="Accomplishment").grid(row=1, column=0, sticky='nw')
         cdp_accomp = tk.Label(frame_canvas_data_display, width=10, anchor='ne',
-                              relief='sunken', textvariable=self.str_cdp_accomp)\
+                              relief='sunken', textvariable=self.str_cdp_accomp) \
             .grid(row=1, column=1, sticky='nesw', padx=5, pady=5)
         cdp_slippage_label = tk.Label(frame_canvas_data_display, text='Slippage').grid(row=2, column=0, sticky="nw")
         cdp_slippage = tk.Label(frame_canvas_data_display, width=10, anchor='ne',
-                                relief='sunken', textvariable=self.str_slippage)\
+                                relief='sunken', textvariable=self.str_slippage) \
             .grid(row=2, column=1, sticky='nesw', padx=5, pady=5)
 
         # Summary
@@ -87,16 +94,27 @@ class Timeline(tk.Frame):
         frame_summary.grid(row=1, column=0, sticky='new', padx=10, pady=5)
         frame_summary.grid_columnconfigure(1, weight=1)
 
-        summary_total_suspended_days_label = tk.Label(frame_summary, text='Total Suspended Days').grid(row=0, column=0)
-        summary_total_suspended = tk.Label(frame_summary, width=10, anchor='ne',
-                                           relief='sunken', textvariable=self.str_summary_total_suspended)\
+        summ_total_susp_days_label = tk.Label(frame_summary, text='Total Suspended Days') \
+            .grid(row=0, column=0, sticky='nw')
+        summ_total_susp = tk.Label(frame_summary, width=10, anchor='ne',
+                                   relief='sunken', textvariable=self.str_summary_total_suspended) \
             .grid(row=0, column=1, padx=5, pady=5)
 
-        summary_total_suspension_order_label = tk.Label(frame_summary, text='Total Suspension Orders')\
-            .grid(row=1, column=0)
-        summary_total_suspension_order = tk.Label(frame_summary, width=10, anchor='ne', relief='sunken',
-                                                  textvariable=self.str_summary_total_suspension_order) \
+        summ_total_susp_order_label = tk.Label(frame_summary, text='Total Suspension Orders') \
+            .grid(row=1, column=0, sticky='nw')
+        summ_total_susp_order = tk.Label(frame_summary, width=10, anchor='ne', relief='sunken',
+                                         textvariable=self.str_summary_total_suspension_order) \
             .grid(row=1, column=1, padx=5, pady=5)
+        summ_orig_completion_days_label = tk.Label(frame_summary, text='Original Completion Days') \
+            .grid(row=2, column=0, sticky='nw')
+        summ_orig_completion_days = tk.Label(frame_summary, width=10, anchor='ne', relief='sunken',
+                                             textvariable=self.str_summ_orig_completion_days) \
+            .grid(row=2, column=1, padx=5, pady=5)
+        summ_rev_completion_days_label = tk.Label(frame_summary, text='Revised Completion Days') \
+            .grid(row=3, column=0, sticky='nw')
+        summ_rev_completion_days = tk.Label(frame_summary, width=10, anchor='ne', relief='sunken',
+                                            textvariable=self.str_summ_rev_completion_days) \
+            .grid(row=3, column=1, padx=5, pady=5)
 
         # ==========================================================
         # Canvas
@@ -113,8 +131,11 @@ class Timeline(tk.Frame):
         by incorporating all the suspensions.
         """
 
-        # Breaks the projected based on suspensions
         self.str_summary_total_suspension_order.set(len(self.suspensions))
+        self.str_summ_orig_completion_days\
+            .set(self.projected_accomplishment[len(self.projected_accomplishment)-1]['time'])
+
+        # Breaks the projected based on suspensions
         for i in range(len(self.suspensions)):
             # Temporary holder for the projected timeline
             temp_projected = []
@@ -163,14 +184,24 @@ class Timeline(tk.Frame):
 
             self.projected_accomplishment = temp_projected
         self.str_summary_total_suspended.set(self.total_suspension_duration)
+        # Total number of days to complete
+
+        total_days = temp_projected[len(temp_projected)-1]['time']
+        self.str_summ_rev_completion_days.set(total_days)
 
     def plot_timeline(self):
+        """
+        Plot everything.
+        """
         self.recalculate()
         self.display_grid()
         self.plot(self.projected_accomplishment, '#0000ff')
         self.plot(self.actual_accomplishment, '#ff0000')
 
     def display_grid(self):
+        """
+        Displays the grid lines of the graph.
+        """
         grid_color = '#808080'
         can = self.canvas
         grid_count_vertical = 10
@@ -184,7 +215,10 @@ class Timeline(tk.Frame):
                             w - self.canvas_right_margin,
                             i * grid_height + self.canvas_top_margin,
                             fill=grid_color,
-                            dash=(2, 2))
+                            dash=(2, 2), width=0.01)
+            can.create_text(10, self.canvas_height - (i+1) * grid_height, text=i * 10, anchor='nw', offset='15, 0')
+        can.create_text(10, self.canvas_height - (grid_count_vertical + 1) * grid_height, text=100, anchor='nw', offset='5, 0')
+
         can.create_line(self.canvas_left_margin,
                         self.canvas_top_margin,
                         w - self.canvas_right_margin,
@@ -207,6 +241,11 @@ class Timeline(tk.Frame):
                         width=2)
 
     def plot(self, data, line_fill_color):
+        """
+        Plot a given data, either the projected or actual accomplishment.
+        :param data: The data to be plotted (e.g. projected accomplishment).
+        :param line_fill_color: The desired color for the line segments.
+        """
         can = self.canvas
 
         min_y = data[0]['accomp']
@@ -279,6 +318,11 @@ class Timeline(tk.Frame):
         self.master.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
     def calculate_slippage(self, time):
+        """
+        Calculates the slippage (positive or negative) at a given point
+        in time.
+        :param time: The number of days elapsed from the start of the project.
+        """
         actual = self.get_accomplishment_at(time, self.actual_accomplishment)
         projected = self.get_accomplishment_at(time, self.projected_accomplishment)
         self.slippage = round((actual - projected), 2)
