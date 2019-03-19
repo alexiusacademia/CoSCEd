75300a1a0fa160894b1ee49680ce6281574fa2d4
alexiusacademia <alexius.academia@gmail.com>
2019-03-19 19:46:48 +0800

    Added view

--- visualization_page.py
+++ visualization_page.py
@@ -22,20 +22,44 @@ class Timeline(tk.Frame):
         self.grid_rowconfigure(1, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
-        lblLeft_controls = tk.Label(self, text="Controls")
-        lblLeft_controls.configure(bg="#ff0000")
-        lblLeft_controls.grid(column=0, row=0, sticky='nesw')
+        self.total_suspension_duration = 0
 
-        lbl = tk.Label(self, text="Time Frame")
-        lbl.grid(column=1, row=0)
+        canvas_title = tk.Label(self, text="Time Frame")
+        canvas_title.grid(column=1, row=0)
 
+        # ===========================================================
+        # String Variables
+        self.str_cdp_time = tk.StringVar()
+        self.str_cdp_accomp = tk.StringVar()
+        self.str_summary_total_suspensions = tk.StringVar()
+        #===========================================================
+        left_panel = tk.Frame(self)
+        left_panel.grid(row=0, column=0, rowspan=2, sticky='nesw')
+
+        # Canvas data display
+        frame_canvas_data_display = tk.LabelFrame(left_panel, text="Canvas Data")
+        frame_canvas_data_display.grid(row=0, column=0, sticky="new", padx=10, pady=5)
+
+        cdp_time_label = tk.Label(frame_canvas_data_display, text="Time").grid(row=0, column=0, sticky="nw")
+        cdp_time = tk.Label(frame_canvas_data_display, text='', width=10, anchor='ne', textvariable=self.str_cdp_time).grid(row=0, column=1, sticky="new", padx=10)
+        cdp_accomp_label = tk.Label(frame_canvas_data_display, text="Accomplishment").grid(row=1, column=0, sticky='nw')
+        cdp_accomp = tk.Label(frame_canvas_data_display, width=10, anchor='ne', textvariable=self.str_cdp_accomp).grid(row=1, column=1, sticky='new', padx=10)
+
+        # Summary
+        frame_summary = tk.LabelFrame(left_panel, text='Summary')
+        frame_summary.grid(row=1, column=0, sticky='new', padx=10, pady=5)
+
+        summary_total_suspension_label = tk.Label(frame_summary, text='Total Suspension').grid(row=0, column=0)
+        summary_total_suspension = tk.Entry(frame_summary, textvariable=self.str_summary_total_suspensions).grid(row=0, column=1, padx=5, pady=5)
+
+        #===========================================================
+        # Canvas
         canvas_frame = tk.Frame(self)
         canvas_frame.grid(column=1, row=1)
-
-        self.canvas = tk.Canvas(canvas_frame)
+        self.canvas = tk.Canvas(canvas_frame, cursor='target')
         self.canvas.configure(width=self.canvas_width, height=self.canvas_height, bg="#ffffff")
-
         self.canvas.grid(column=1, row=1, sticky='nesw')
+        #===========================================================
 
     def recalculate(self):
         """
@@ -54,6 +78,8 @@ class Timeline(tk.Frame):
             start_suspended = self.suspensions[i]['start']
             duration_suspended = self.suspensions[i]['duration']
 
+            self.total_suspension_duration += duration_suspended
+
             for j in range(len(self.projected_accomplishment) - 1):
 
                 t1 = self.projected_accomplishment[j]['time']
@@ -66,7 +92,7 @@ class Timeline(tk.Frame):
                     "accomp": accomp1
                 })
 
-                if (t1 < start_suspended and start_suspended < t2):
+                if (t1 < start_suspended) and (start_suspended < t2):
                     total_suspensions += duration_suspended
 
                     # Now, calculate the corresponding accomplishment
@@ -89,8 +115,7 @@ class Timeline(tk.Frame):
             })
 
             self.projected_accomplishment = temp_projected
-
-        # Recalculates the plot for the actual accomplishment
+        self.str_summary_total_suspensions.set(self.total_suspension_duration)
 
     def hover(self, event):
         point = (event.widget.find_closest(event.x, event.y))[0]
@@ -98,8 +123,9 @@ class Timeline(tk.Frame):
         for p in self.points:
             if p['point'] == point:
                 data = p
-        if data != None:
-            print(data)
+        if data is not None:
+            self.str_cdp_time.set(data['time'])
+            self.str_cdp_accomp.set(data['accomp'])
 
     def plot_timeline(self):
         self.recalculate()
@@ -188,6 +214,21 @@ class Timeline(tk.Frame):
                 "time" : x1,
                 "accomp" : y1
             })
+        # Plot the last node
+        x1 = data[rows-1]['time']
+        y1 = data[rows-1]['accomp']
+        pt = can.create_rectangle(x1 * width_factor - 2 + left_margin,
+                                  self.canvas_height - y1 * height_factor - 2 - bottom_margin,
+                                  x1 * width_factor + 2 + left_margin,
+                                  self.canvas_height - y1 * height_factor + 2 - bottom_margin,
+                                  fill=line_fill_color,
+                                  tags='point')
+        self.canvas.tag_bind(pt, '<Enter>', self.hover)
+        self.points.append({
+            "point": pt,
+            "time": x1,
+            "accomp": y1
+        })
 
     def center_window(self):
         w = self.master.winfo_screenwidth()
