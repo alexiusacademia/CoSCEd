e25a83193b9eeef896820e825f32c8606874feff
Alexius Academia <alexius.academia@gmail.com>
2019-04-02 18:53:57 +0800

    Projected editor: Able to parse the given filename to json.

--- dialogs/projected.py
+++ dialogs/projected.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+import json
 
 
 class ProjectedAccomplishmentDialog:
@@ -18,9 +19,26 @@ class ProjectedAccomplishmentDialog:
             messagebox.showinfo("Error", "No file opened.\n"
                                          "A file is necessary to be opened or created to show this editor.")
         else:
-            print('Filename:', self.filename)
-
             self.top = tk.Toplevel(self.parent)
             self.top.title = "Projected Accomplishment Editor"
             self.top.transient(self.parent)
             self.top.grab_set()
+
+            # Parse the file
+            json_string = ''
+            file = open(self.filename, 'r')
+            for line in file.readlines():
+                json_string += line
+            file.close()
+
+            # Convert string to json
+            project_json = json.loads(json_string)
+
+            project_json['projected'].append({
+                'time': 30,
+                'accomp': 25
+            })
+
+            # Convert the json object to python object
+            with open(self.filename, 'w') as output_file:
+                json.dump(project_json, output_file, indent=4)
