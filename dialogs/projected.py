bfa2eebac1084f3a4597203b27f6d714669df885
Alexius Academia <alexius.academia@gmail.com>
2019-04-02 18:20:20 +0800

    Set the dialog as modal.

--- dialogs/projected.py
+++ dialogs/projected.py
@@ -2,8 +2,13 @@ import tkinter as tk
 
 
 class ProjectedAccomplishmentDialog:
+    top = None
+
     def __init__(self, parent):
         self.parent = parent
 
     def show(self):
-        top = self.top = tk.Toplevel(self.parent)
+        self.top = tk.Toplevel(self.parent)
+        self.top.title = "Projected Accomplishment Editor"
+        self.top.transient(self.parent)
+        self.top.grab_set()
