30df7770aa7982edcac8586ddc4fc7ec9ff6663e
alexiusacademia <alexius.academia@gmail.com>
2019-04-04 17:07:43 +0800

    Added icon.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import json
+import os
 
 from visualization_page import *
 
 if __name__ == '__main__':
     root = tk.Tk()
+
+    root.iconbitmap(os.getcwd() + '\\res\\icons\\icon.ico')
 
     timeline = Timeline(root)
 
