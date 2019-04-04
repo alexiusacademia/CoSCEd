5441eb3d6284fe34b5d8a42a6a7b8b075c2a5fe5
alexiusacademia <alexius.academia@gmail.com>
2019-04-04 17:22:31 +0800

    Added type checking in editor and display error to status bar.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from visualization_page import *
 if __name__ == '__main__':
     root = tk.Tk()
 
-    root.iconbitmap(os.getcwd() + '\\res\\icons\\icon.ico')
+    # root.iconbitmap(os.getcwd() + '\\res\\icons\\icon.ico')
 
     timeline = Timeline(root)
 
