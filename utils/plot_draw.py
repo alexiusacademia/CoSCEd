5f148e2a184203f14551f1554695f33e6cb71cd5
Alexius Academia <alexius.academia@gmail.com>
2019-05-26 10:00:15 +0800

    Replaced separate objects with the one draw_object.

--- utils/plot_draw.py
+++ utils/plot_draw.py
@@ -4,36 +4,30 @@ from PIL import Image, ImageDraw
 class PlotDraw:
     path = ''
     size = ()
-    projected = []
-    actual = []
+    draw_object = {}
 
     def __init__(self, fp, size):
         self.path = fp
         self.size = size
 
-    def set_projected(self, projected):
-        """
-        Sets the coordinates for plotting the projected line.
-        :param projected: List of tuples (x, y)
-        :return:
-        """
-        self.projected = projected
-
-    def set_actual(self, actual):
+    def set_draw_object(self, do):
         """
         Sets the coordinates for plotting the actual line.
         :param actual: List of tuples (x, y)
         :return:
         """
-        self.actual = actual
+        self.draw_object = do
 
     def draw_image(self):
         img = Image.new('RGB', self.size, color='white')
 
         draw = ImageDraw.Draw(img)
 
-        draw.line(self.projected, fill='blue', width=1)
-        draw.line(self.actual, fill='red', width=1)
+        projected = self.draw_object['projected']
+        draw.line(projected, fill='blue', width=1)
+
+        actual = self.draw_object['actual']
+        draw.line(actual, fill='red', width=1)
 
         img.save(self.path, quality=100)
         img.resize((self.size[0]*2, self.size[1]*2), Image.ANTIALIAS)
